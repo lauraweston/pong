b933000b4a3438b375a9a04737fc311f5a6f18e7
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-12-05 18:10:00 +0000

    combined features

--- public/js/paddle.js
+++ public/js/paddle.js
@@ -1,11 +1,10 @@
-// var canvas = document.getElementById('canvas');
-// var context = canvas.getContext('2d');
+
 
 function Paddle(context, x, y) {
   this.context = context;
   this.color = "red";
-  this.width = 30;
-  this.height = 100;
+  this.width = 15;
+  this.height = 70;
   this.x = x;
   this.y = y;
 }
@@ -19,26 +18,5 @@ Paddle.prototype.update = function(value) {
   this.y += value;
 };
 
-// var paddle1 = new Paddle(context, 550, 150);
-//
-// function draw(paddle) {
-//   context.clearRect(0, 0, 600, 400);
-//   paddle.draw();
-// }
-//
-// var FPS = 30;
-// setInterval(function() {
-//   update(paddle1);
-//   draw(paddle1);
-// }, 1000/FPS);
-
-function update(paddle) {
-  if (keydown.up) {
-    paddle.update(-5);
-  }
-  if (keydown.down) {
-    paddle.update(5);
-  }
-}
 
 module.exports = Paddle;
--- public/js/pong.js
+++ public/js/pong.js
@@ -3,26 +3,38 @@ window.webkitRequestAnimationFrame ||
 window.mozRequestAnimationFrame ||
 function (callback) {window.setTimeout(callback, 10000 / 60)};
 
-
 (function init(){
   var canvas = document.getElementById("canvas");
   context = canvas.getContext('2d');
   game = new GameBox();
   ball = new Ball();
+  paddle1 = new Paddle(context, 550, 150);
   animate(play);
 })();
 
 function play(){
-  draw()
-  update()
-  animate(play)
-};
+  draw();
+  update();
+  animate(play);
+}
 
 var draw = function(){
-  game.draw()
-  ball.draw()
+  game.draw();
+  ball.draw();
+  paddle1.draw();
 }
 
 var update = function(){
-  ball.update()
+  ball.update();
+  updatePaddle(paddle1)
+}
+
+
+function updatePaddle(paddle) {
+  if (keydown.up) {
+    paddle.update(-5);
+  }
+  if (keydown.down) {
+    paddle.update(5);
+  }
 }
