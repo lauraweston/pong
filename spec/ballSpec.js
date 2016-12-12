bb535252733ffac0baadce8cb164c1c655a356ec
elizabethvenner <elizabethvenner@googlemail.com>
2016-12-12 15:42:09 +0000

    add tests for ball going out of play and for getting player scores

--- spec/ballSpec.js
+++ spec/ballSpec.js
@@ -1,95 +1,17 @@
-// var Ball = require('../public/js/ball.js');
-// var Paddle = require('../public/js/paddle.js');
-// var Player = require('../public/js/player.js');
-//
-// describe("Ball", function(){
-//   var player;
-//   var paddle;
-//   beforeEach(function(){
-//     gameBall = new Ball();
-//     paddle1 = new Paddle(570, 150);
-//     paddle2 = new Paddle(15, 150);
-//     player1 = new Player(paddle1);
-//     player2 = new Player(paddle2);
-//   });
-//
-//   it("has a x, y coordinates", function(){
-//     expect(gameBall.x).toEqual(300);
-//     expect(gameBall.y).toEqual(20);
-//   });
-//
-//   it("has a x, y coordinates velocity speed", function(){
-//     expect(gameBall.xSpeed).toEqual(3);
-//     expect(gameBall.ySpeed).toEqual(2);
-//   });
-//
-//   it("has a x, y coordinates that change on update by 3px", function(){
-//     gameBall.update(player1, player2);
-//     expect(gameBall.x).toEqual(303);
-//     expect(gameBall.y).toEqual(22);
-//   });
-//
-//   describe("changes course when hits wall", function(){
-//
-//     beforeEach(function(){
-//       for(var i = 0; i < 210;  i++) {
-//         gameBall.update(player1, player2);
-//       }
-//     });
-//
-//   it("changes course when it hits a wall", function(){
-//     expect(gameBall.xSpeed).toEqual(-3);
-//     expect(gameBall.ySpeed).toEqual(-2);
-//   });
-//
-//   });
-//
-//   // describe("changes course when hits left paddle", function() {
-//   //   it("changes x and y coordinates when it hits the paddle", function() {
-//   //     for(var i = 0; i < 290;  i++) {
-//   //       gameBall.update(player1, player2);
-//   //     }
-//   //     expect(gameBall.xSpeed).toEqual(3);
-//   //     expect(gameBall.ySpeed).toEqual(2);
-//   //   });
-//   // });
-//
-//   describe("resets if ball passes through paddle side", function(){
-//     var paddle3 = new Paddle(0, 0);
-//     var player3 = new Player(paddle3)
-//
-//     beforeEach(function(){
-//       for(var i = 0; i < 900;  i++) {
-//         gameBall.update(player3, player2);
-//       }
-//     });
-//
-//     it("resets x and y coordinates when paddle fails", function(){
-//
-//       expect(gameBall.x).toEqual(300);
-//       expect(gameBall.y).toEqual(20);
-//     });
-//
-//     it("resets x and y coordinates when paddle fails", function(){
-//       expect(gameBall.xSpeed).toEqual(3);
-//       expect(gameBall.ySpeed).toEqual(2);
-//     });
-//   });
-//
-//   describe("increments", function() {
-//     it("player 1 score", function() {
-//       for(var i = 0; i < 290; i++) {
-//         gameBall.update(player1, player2);
-//       }
-//       expect(player1.score).toEqual(1);
-//     });
-//     it("player 2 score", function() {
-//       var paddle3 = new Paddle(0, 0);
-//       var player3 = new Player(paddle3);
-//       for(var i = 0; i < 100;  i++) {
-//         gameBall.update(player3, player2);
-//       }
-//       expect(player2.score).toEqual(1);
-//     });
-//   });
-// });
+var Ball = require('../public/js/ball.js');
+var Paddle = require('../public/js/paddle.js');
+var Player = require('../public/js/player.js');
+
+describe("Ball", function(){
+  var player;
+  var paddle;
+  beforeEach(function(){
+    gameBall = new Ball();
+    paddle1 = new Paddle(570, 150);
+    paddle2 = new Paddle(15, 150);
+    player1 = new Player(paddle1);
+    player2 = new Player(paddle2);
+  });
+
+
+});
--- spec/serverGameControllerSpec.js
+++ spec/serverGameControllerSpec.js
@@ -14,9 +14,17 @@ describe("GameController", function(){
     ball = new Ball();
     paddle1 = new Paddle(15, 150);
     paddle2 = new Paddle(570, 150);
-    player1 = new Player(paddle1);
-    player2 = new Player(paddle2);
+    player1 = new Player(paddle1, 1);
+    player2 = new Player(paddle2, 2);
     gameController = new GameController(ball, player1, player2, gameLoopTickCallback);
+  });
+
+  describe("ball moves", function() {
+    it("when game updates", function(){
+        gameController.update();
+        expect(gameController.ball.x).toEqual(303);
+        expect(gameController.ball.y).toEqual(22);
+      });
   });
 
   describe("ball changes course", function() {
@@ -44,17 +52,53 @@ describe("GameController", function(){
       gameController.update();
       expect(gameController.ball.xSpeed).toBeLessThan(0);
     });
-    it("hits top wall", function() {
+    it("when hits top wall", function() {
       gameController.ball.x = (gameController.gameBox/2);
       gameController.ball.y = (gameController.gameBox.y + ball.ySpeed);
       gameController.update();
       expect(gameController.ball.ySpeed).toBeGreaterThan(0);
     });
-    it("hits bottom wall", function() {
+    it("when hits bottom wall", function() {
       gameController.ball.x = (gameController.gameBox/2);
       gameController.ball.y = (gameController.gameBox.height - ball.ySpeed);
       gameController.update();
       expect(gameController.ball.ySpeed).toBeLessThan(0);
+    });
+  });
+
+  describe("ball goes out of play", function() {
+    describe("when misses player1 paddle", function() {
+      beforeEach(function() {
+        gameController.ball.x = (gameController.gameBox.x - gameController.ball.xSpeed);
+        gameController.ball.y = (gameController.gameBox.y - gameController.ball.ySpeed);
+      });
+      it("ball resets", function() {
+        gameController.update();
+        expect(gameController.ball.x).toEqual(300);
+        expect(gameController.ball.y).toEqual(20);
+      });
+      it("increments player2's score", function() {
+        gameController.update();
+        expect(player2.score).toEqual(1);
+      });
+    });
+    describe("when misses player2 paddle", function() {
+      beforeEach(function() {
+        gameController.ball.x = (gameController.gameBox.width - gameController.ball.xSpeed);
+        gameController.ball.y = (gameController.gameBox.y - gameController.ball.ySpeed);
+      });
+      it("ball resets", function() {
+        gameController.update();
+        expect(gameController.ball.x).toEqual(300);
+        expect(gameController.ball.y).toEqual(20);
+      });
+      it("increments player1's score", function() {
+        gameController.update();
+        expect(player1.score).toEqual(1);
+      });
+    });
+    it("shows player's scores", function() {
+      expect(gameController.getPlayerScores()).toEqual({player1:{id: 1, score: 0}, player2:{id: 2, score:0}});
     });
   });
 });
