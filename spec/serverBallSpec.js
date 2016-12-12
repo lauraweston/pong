08c9faa01b5397c1ddecf68ca5f2f14fa2f9f591
elizabethvenner <elizabethvenner@googlemail.com>
2016-12-12 11:02:26 +0000

    add tests for serverBall/js

--- spec/serverBallSpec.js
+++ spec/serverBallSpec.js
@@ -13,4 +13,14 @@ describe('serverBall', function() {
     expect(serverBall.xSpeed).toEqual(-3);
   });
 
+  it("changes direction when bounches off wall", function() {
+    serverBall.bounceWall();
+    expect(serverBall.ySpeed).toEqual(-2);
+  });
+
+  it("returns the balls coordinates", function() {
+    expect(serverBall.getCoordinates()).toEqual({x: 300, y: 20});
+  });
+
+
 });
