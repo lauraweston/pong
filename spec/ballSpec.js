1d89080a412a2d23d3e8fc5ffed6cb44e74c4aa9
elizabethvenner <elizabethvenner@googlemail.com>
2016-12-12 16:04:30 +0000

    add tests for client ball

--- spec/ballSpec.js
+++ spec/ballSpec.js
@@ -1,16 +1,17 @@
 var Ball = require('../public/js/ball.js');
-var Paddle = require('../public/js/paddle.js');
-var Player = require('../public/js/player.js');
+var ball;
+var context;
 
 describe("Ball", function(){
-  var player;
-  var paddle;
   beforeEach(function(){
-    gameBall = new Ball();
-    paddle1 = new Paddle(570, 150);
-    paddle2 = new Paddle(15, 150);
-    player1 = new Player(paddle1);
-    player2 = new Player(paddle2);
+    ball = new Ball(context);
+    console.log(ball);
+  });
+  it("sets the coordinates", function() {
+    ball.setCoordinates({x: 303, y: 22});
+    console.log(ball.x);
+    expect(ball.x).toEqual(303);
+    expect(ball.y).toEqual(22);
   });
 
 
