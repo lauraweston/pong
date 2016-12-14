1f5e90d9681ef7be02b827c12c6ff3d3e1aa1ed8
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-12-14 11:12:04 +0000

    some refractoring

--- public/bundle.js
+++ public/bundle.js
@@ -44,14 +44,7 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var GameController = __webpack_require__(1);
-	var GameBox = __webpack_require__(2);
-	var Ball = __webpack_require__(3);
-	var Player = __webpack_require__(4);
-	var Paddle = __webpack_require__(5);
-	var keydown = __webpack_require__(6);
-	var animate = __webpack_require__(7);
-	__webpack_require__(8);
+	var helper = __webpack_require__(1);
 	var socket;
 	var localPlayer;
 	var opponent;
@@ -62,7 +55,6 @@
 	var gameController;
 	var gameEnded = false;
 	var audio = new Audio("pongSound.mp3");
-
 	var pong = document.getElementById('pong');
 	var signInForm = document.getElementById('signIn');
 	var newPlayerName = document.getElementById('playerName');
@@ -161,15 +153,6 @@
 	      animate(gameLoop);
 	      return;
 	    }, 5000);
-
-
-	    // for(i = 5; i < 1; i--;){
-	    //   (setTimeout(function(){
-	    //     gameStart.innerHTML = i;
-	    //   },1000)()
-	    // }
-	    //check out self-calling functions as I think this needs to be one
-
 	  }
 
 
@@ -252,6 +235,23 @@
 
 /***/ },
 /* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	function Helper(){
+	var GameController = __webpack_require__(2);
+	var GameBox = __webpack_require__(3);
+	var Ball = __webpack_require__(4);
+	var Player = __webpack_require__(5);
+	var Paddle = __webpack_require__(6);
+	var keydown = __webpack_require__(7);
+	var animate = __webpack_require__(8);
+	__webpack_require__(9);
+	};
+	module.exports = Helper;
+
+
+/***/ },
+/* 2 */
 /***/ function(module, exports) {
 
 	var GameController = function(ball, gamebox, localPlayer, opponent){
@@ -296,7 +296,7 @@
 
 
 /***/ },
-/* 2 */
+/* 3 */
 /***/ function(module, exports) {
 
 	function GameBox(context){
@@ -325,7 +325,7 @@
 
 
 /***/ },
-/* 3 */
+/* 4 */
 /***/ function(module, exports) {
 
 	var Ball = function(context){
@@ -349,7 +349,7 @@
 
 
 /***/ },
-/* 4 */
+/* 5 */
 /***/ function(module, exports) {
 
 	function Player (paddle, context, name) {
@@ -379,7 +379,7 @@
 
 
 /***/ },
-/* 5 */
+/* 6 */
 /***/ function(module, exports) {
 
 	function Paddle(x, y, context) {
@@ -429,7 +429,7 @@
 
 
 /***/ },
-/* 6 */
+/* 7 */
 /***/ function(module, exports) {
 
 	 window.keydown = {};
@@ -451,7 +451,7 @@
 
 
 /***/ },
-/* 7 */
+/* 8 */
 /***/ function(module, exports) {
 
 	var animate = window.requestAnimationFrame ||
@@ -463,7 +463,7 @@
 
 
 /***/ },
-/* 8 */
+/* 9 */
 /***/ function(module, exports) {
 
 	/*
---
+++ src/client/pong.js
@@ -0,0 +1,185 @@
+var GameController = require('./gameController.js');
+var GameBox = require('./gameBox.js');
+var Ball = require('./ball.js');
+var Player = require('./player.js');
+var Paddle = require('./paddle.js');
+var keydown = require('./../../lib/key_status.js');
+var animate = require('./animationFrame.js');
+require('./../../lib/jquery.hotkeys.js');
+var socket;
+var localPlayer;
+var opponent;
+var localBall;
+var context;
+var canvas;
+var gameBox;
+var gameController;
+var gameEnded = false;
+var lastPaddleMove = 0;
+var audio = new Audio("pongSound.mp3");
+var pong = document.getElementById('pong');
+var signInForm = document.getElementById('signIn');
+var newPlayerName = document.getElementById('playerName');
+var waiting = document.getElementById('waiting');
+var disconnect = document.getElementById('disconnect');
+var winner = document.getElementById('winner');
+var playAgain = document.getElementById('playAgain');
+
+(function init(){
+  canvas = document.getElementById("canvas");
+  context = canvas.getContext('2d');
+  gameBox = new GameBox(context);
+  socket = io.connect(getUrl());
+  audio.play();
+  setEventHandlers();
+})();
+
+function setEventHandlers() {
+  socket.on("connect", onSocketConnected);
+  socket.on("start game", startGame);
+  socket.on("server moves ball", onServerMovesBall);
+  socket.on("server moves player", onServerMovePlayer);
+  socket.on("server updates scores", onServerUpdatesScores);
+  socket.on("game won", declareWinner);
+  socket.on("disconnect", onSocketDisconnect);
+  socket.on("remove player", removePlayer);
+}
+
+function onSocketConnected() {
+  console.log("Connected to socket server");
+}
+
+function startGame(gameData){
+  pong.style.display = 'none'
+  canvas.style.display = 'inline';
+  waiting.style.display = 'none';
+  disconnect.style.display = 'none';
+  winner.innerHTML = "";
+  winner.style.display = 'none';
+  for(var i = 0; i < gameData.players.length; i++) {
+    var player = gameData.players[i];
+    var paddle = new Paddle(player.x, player.y, context);
+    if (player.id === myId()) {
+      localPlayer = new Player(paddle, context, player.name);
+      localPlayer.id = myId();
+    } else {
+      opponent = new Player(paddle, context, player.name);
+      opponent.id = player.id;
+    }
+  }
+  localBall = new Ball(context);
+  localBall.setCoordinates(gameData.ballCoordinates);
+  gameController = new GameController(localBall, gameBox, localPlayer, opponent);
+  gameController.resetGame();
+  countdown();
+  draw();
+}
+
+
+function onServerMovesBall(data) {
+  localBall.setCoordinates(data);
+}
+
+function onServerMovePlayer(data) {
+  if (data.id === opponent.id) {
+    opponent.paddle.setY(data.y);
+  }
+}
+
+function onServerUpdatesScores(data) {
+  gameController.setScores(data);
+}
+
+function declareWinner(data){
+  gameController.endGame();
+  var textHolder = document.createElement("h2")
+  textHolder.innerHTML = data.winner.name + " wins!";
+  winner.appendChild(textHolder)
+  winner.style.display = 'inline';
+  playAgain.style.display = 'inline';
+  gameStart = document.getElementById('countdown');
+  gameStart.innerHTML = "Game Over!";
+}
+
+function onSocketDisconnect() {
+  console.log("Disconnected from socket server");
+}
+
+function removePlayer(){
+  gameController.endGame();
+  disconnect.style.display = "inline";
+  winner.style.display = 'none';
+  gameStart = document.getElementById('countdown');
+  gameStart.innerHTML = "Game Over!"
+}
+
+function myId() {
+  return socket.io.engine.id;
+}
+
+function countdown() {
+  var gameStart = document.getElementById('countdown');
+  setTimeout(function() {gameStart.innerHTML="4"}, 1000);
+  setTimeout(function() {gameStart.innerHTML="3"}, 2000);
+  setTimeout(function() {gameStart.innerHTML="2"}, 3000);
+  setTimeout(function() {gameStart.innerHTML="1"}, 4000);
+  setTimeout(function() {
+    audio.pause();
+    gameStart = document.getElementById('countdown');
+    gameStart.innerHTML = "Play!";
+    animate(gameLoop);
+    return;
+  }, 5000);
+}
+
+function gameLoop(){
+  checkForPaddleMove();
+  draw();
+  if (gameController.isGameEnded === false) {
+    animate(gameLoop);
+  }
+}
+
+function draw(){
+  gameController.drawGame();
+}
+
+function checkForPaddleMove(){
+  var timeNow = new Date();
+  var timeSinceLastMove = timeNow - lastPaddleMove;
+  if (timeSinceLastMove < 15) {
+    return;
+  }
+  var paddleMoved = false;
+  if (keydown.down) {
+    localPlayer.paddle.moveDown();
+    paddleMoved = true;
+  } else if (keydown.up) {
+    localPlayer.paddle.moveUp();
+    paddleMoved = true;
+  }
+  if (paddleMoved) {
+    socket.emit("client moves player", {y: localPlayer.paddle.getY()});
+    lastPaddleMove = timeNow;
+  }
+}
+
+signInForm.onsubmit = function(event){
+  event.preventDefault();
+  waiting.style.display = 'inline';
+  signInForm.style.display = 'none';
+  socket.emit('player sign in', {playerName: newPlayerName.value});
+}
+
+playAgain.onclick = function() {
+  socket.emit("play again");
+  disconnect.style.display = 'none';
+  waiting.style.display = 'inline';
+  winner.style.display = 'none';
+  playAgain.style.display = 'none';
+  canvas.style.display = 'none';
+}
+
+function getUrl() {
+  return location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
+}
