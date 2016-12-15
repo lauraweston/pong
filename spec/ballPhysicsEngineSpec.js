f458ca0e72e263d75657177d756b561210436f3e
Laura Weston <laura.weston@outlook.com>
2016-12-15 16:44:14 +0000

    Tests for ballPhysicsEngine

--- spec/ballPhysicsEngineSpec.js
+++ spec/ballPhysicsEngineSpec.js
@@ -1,20 +1,36 @@
-// var ballPhysicsEngine = require('../src/server/game/ballPhysicsEngine.js');
-//
-//
-// describe("ballPhysicsEngine", function() {
-//   var ball;
-//   beforeEach(function() {
-//     ball = jasmine.createSpyObj('ball', ['y', 'bounceWall', 'x', 'bouncePaddle', 'reset']);
-//     gameBox = jasmine.createSpyObj('gameBox', ['x', 'y', 'height', 'width']);
-//     paddle = jasmine.createSpyObj('paddle', ['x', 'y', 'height', 'width']);
-//     player1 = jasmine.createSpyObj('player1', ['increaseScore', paddle]);
-//   });
-//
-//   describe("ballHitsWall", function(){
-//     it("calls bounceWall on ball when ball hits", function() {
-//
-//
-//     });
-//   });
-//
-// });
+var ballPhysicsEngine = require('../src/server/game/ballPhysicsEngine.js');
+var ballHitsWall = ballPhysicsEngine.ballHitsWall;
+var Ball = require("../src/server/game/serverBall.js");
+
+describe("ballPhysicsEngine", function() {
+  var ball = new Ball();
+  var gameBox = {
+                  height: 400,
+                  width:  600,
+                  x:        0,
+                  y:        0
+  };
+
+  describe("ballHitsWall", function(){
+    beforeEach(function() {
+      spyOn(ball, 'bounceWall');
+    });
+
+    it("calls bounceWall on ball when ball hits top wall", function() {
+      ball.y = gameBox.y;
+      ballHitsWall(ball, gameBox);
+      expect(ball.bounceWall).toHaveBeenCalled();
+    });
+    it("calls bounceWall on ball when ball hits bottom wall", function() {
+      ball.y = gameBox.height;
+      ballHitsWall(ball, gameBox);
+      expect(ball.bounceWall).toHaveBeenCalled();
+    });
+    it("does not call bounceWall on ball when ball does not hit a wall", function() {
+      ball.y = 1;
+      ballHitsWall(ball, gameBox);
+      expect(ball.bounceWall).not.toHaveBeenCalled();
+    });
+  });
+
+});
