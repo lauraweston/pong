28ae4179f285c2029ee4a70815391918c9306298
elizabethvenner <elizabethvenner@googlemail.com>
2016-12-15 16:08:17 +0000

    start witing tests of ballPhysicsEngine

---
+++ spec/ballPhysicsEngineSpec.js
@@ -0,0 +1,20 @@
+// var ballPhysicsEngine = require('../src/server/game/ballPhysicsEngine.js');
+//
+//
+// describe("ballPhysicsEngine", function() {
+//   var ball;
+//   beforeEach(function() {
+//     ball = jasmine.createSpyObj('ball', ['y', 'bounceWall', 'x', 'bouncePaddle', 'reset']);
+//     gameBox = jasmine.createSpyObj('gameBox', ['x', 'y', 'height', 'width']);
+//     paddle = jasmine.createSpyObj('paddle', ['x', 'y', 'height', 'width']);
+//     player1 = jasmine.createSpyObj('player1', ['increaseScore', paddle]);
+//   });
+//
+//   describe("ballHitsWall", function(){
+//     it("calls bounceWall on ball when ball hits", function() {
+//
+//
+//     });
+//   });
+//
+// });
